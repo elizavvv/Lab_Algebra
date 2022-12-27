91de245439538224e3eac149a8ef36c1c32bfdbd
elizavvv <elizav.terentyeva@gmail.com>
2022-12-27 22:27:48 +0400

    commit2

--- scripts/main.js
+++ scripts/main.js
@@ -1,62 +1,63 @@
         
-        //Вычисление суммы матриц 2х2
+        //Вычисление суммы матриц 3х3
         let display1 = document.querySelector('#matrix1'); 
         let display2 = document.querySelector('#matrix2');
 
-        display1.addEventListener('change', () =>{  
-            display2.addEventListener('change', () =>{
-
-                let a = display1.value.split("\n");
-                
-                for (let i in a) {
-                    a[i] = a[i].split(" ");
-                }
-
-                let b = display2.value.split("\n");
-                
-                for (let i in b) {
-                    b[i] = b[i].split(" ");
-                }  
-
-                        let matrix =[];
-
-                        for (let i in a) {
-                            matrix[i] =[];
-                            for (let j in a[i]) {
-                            
-                                matrix[i][j] = +(a[i][j])+ +(b[i][j]);
-                            }  
+        let S;
+        
+                      function Sum() {
+                            let a = display1.value.split("\n");
                         
-                        } 
-                        //console.log(matrix[0][0]);
+                            for (let i in a) {
+                                a[i] = a[i].split(" ");
+                            }
+                            console.log(a[1][0]?.value);
+                            let b = display2.value.split("\n");
+                            
+                            for (let i in b) {
+                                b[i] = b[i].split(" ");
+                            }  
 
-                         const S = katex.renderToString("\\begin{pmatrix}" + 
-                        matrix[0][0] +"&"+ matrix[0][1] +"&"+ matrix[0][2] +"\\newline" +
-                        matrix[1][0] +"&"+ matrix[1][1] +"&"+ matrix[1][2] +"\\newline" +
-                        matrix[2][0] +"&"+ matrix[2][1] +"&"+ matrix[2][2] +
-							"\\end{pmatrix}", { throwOnError: false});
+                            let matrix =[];
+                            
+                                    for (let i in a) {
+                                        matrix[i] =[];
+                                        for (let j in a[i]) {
 
-                            document.querySelector("#resSum").innerHTML = S;
-                    
-                console.log(matrix);
+                                            matrix[i][j] = +(a[i][j])+ +(b[i][j]);
+                                        }                                  
+                                    }  
+                                         S = katex.renderToString("\\begin{pmatrix}" + 
+                                        matrix[0][0] +"&"+ matrix[0][1] +"&"+ matrix[0][2] +"\\newline" +
+                                        matrix[1][0] +"&"+ matrix[1][1] +"&"+ matrix[1][2] +"\\newline" +
+                                        matrix[2][0] +"&"+ matrix[2][1] +"&"+ matrix[2][2] + "\\end{pmatrix}", { throwOnError: false});
+                                        document.querySelector('#resSum').innerHTML = S;
+                                        
+                                       
+                        }
+                        display1.addEventListener('change', Sum);
+                        display2.addEventListener('change', Sum);
+                        
 
-         }) })
+/**/
 
         //Вычисление определителя 2х2
+
 
                  let A = document.querySelector('#matrixA'); 
                     A.addEventListener('change', () =>{  
 
-                        
                         let det;
 
                         let a = A.value.split("\n");
                     
                             for (let i in a) {
-                             a[i] = a[i].split(" ");
-                              }
 
-                              det= +a[0][0] * +a[1][1] - +a[1][0] * +a[0][1];
+                             a[i] = a[i].split(" ");
+
+                            }
+
+                            det= +a[0][0] * +a[1][1] - +a[1][0] * +a[0][1];
 
 
                             console.log(a);
@@ -137,6 +138,56 @@
             
         })
 
+        // Произведение матриц 3x3
+
+        let Matr1 = document.querySelector('#matr1'); 
+        let Matr2 = document.querySelector('#matr2');
+
+        let MultM;
+        
+                      function Mult() {
+
+                            let a = Matr1.value.split("\n");
+                        
+                            for (let i in a) {
+                                a[i] = a[i].split(" ");
+                            }
+                           // console.log(a[1][0]?.value);
+
+                            let b = Matr2.value.split("\n");
+                            
+                            for (let i in b) {
+                                b[i] = b[i].split(" ");
+                            }  
+
+                            let matrix =[];
+                            
+                                    for (let i in a) {
+                                        //matrix[i] =[];
+                                        matrix[i]=[];
+                                        for (let j in a[i]) {
+
+                                            matrix[i][j]=0;
+                                            for (k=0; k<3;k++){
+                                                matrix[i][j] += +(a[i][k])* +(b[k][j]);
+                                            }
+                                            
+                                        }                                  
+                                    }  
+                                    console.log(matrix[1][1]);
+                                    
+                                         MultM = katex.renderToString("\\begin{pmatrix}" + 
+                                        matrix[0][0] +"&"+ matrix[0][1] +"&"+ matrix[0][2] +"\\newline" +
+                                        matrix[1][0] +"&"+ matrix[1][1] +"&"+ matrix[1][2] +"\\newline" +
+                                        matrix[2][0] +"&"+ matrix[2][1] +"&"+ matrix[2][2] + "\\end{pmatrix}", { throwOnError: false});
+
+                                        document.querySelector('#resMult').innerHTML = MultM;
+                                        
+                                       
+                        }
+
+                        Matr1.addEventListener('change', Mult);
+                        Matr2.addEventListener('change', Mult);
 
 
 
